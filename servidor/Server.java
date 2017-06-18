b5e3c4d7cdcd51ac293a10104675cc828a65ec99
Rafael Baiolim <rafaelbaiolim@gmail.com>
2017-06-17 22:09:23 -0300

    Commit inicial, servidor e .ino OK

---
+++ servidor/Server.java
@@ -0,0 +1,70 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Random;
+
+public class Server {
+
+    public static String sentence;
+
+    public static String ipSocket = "192.168.25.124";
+    public static int serverPort = 9876;
+    public static int arduinoPort = 9875;
+
+    public static final String ON_MOTOR_1 = "ON_MOTOR_1";
+    public static final String ON_MOTOR_2 = "ON_MOTOR_2";
+    public static final String ON_MOTOR_3 = "ON_MOTOR_3";
+    public static final String OFF_MOTOR_1 = "OFF_MOTOR_1";
+
+    /**
+     * @breath Envia um comando para o arduíno via UDP este método pode ser
+     * implementado junto com os comandos de interface do projeto, utilize
+     * depois que o servidor java estiver iniciado
+     * @param comando Comando a ser executado
+     * @param socket Socket de envio
+     * @param IP_Arduino IP do socket
+     * @throws IOException
+     */
+    public static void enviarComandoArduino(DatagramSocket socket,
+            InetAddress IP_Arduino, String comando)
+            throws IOException {
+        System.out.println("\n>ENVIANDO COMANDO: " + comando);
+        byte[] comandAsByte = comando.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(comandAsByte,
+                comandAsByte.length, IP_Arduino, arduinoPort);
+        socket.send(sendPacket);
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        final String[] comandos = {ON_MOTOR_1, ON_MOTOR_2, ON_MOTOR_3, OFF_MOTOR_1};
+        Random random = new Random();
+
+        //Servidor
+        DatagramSocket serverSocket = new DatagramSocket(null);
+        InetSocketAddress bindAdress = new InetSocketAddress(ipSocket, serverPort);
+        serverSocket.bind(bindAdress);
+	System.out.println("Servidor Iniciado.");
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+
+        while (true) {
+            //recebe o datagrama do cliente
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            sentence = new String(receivePacket.getData());
+            System.out.println(">>RECEIVED: " + sentence);
+
+            //trecho exemplo de requisições para o Arduíno, pode ser removido
+            //desta parte do algoritmo, envia um comando aletório:
+            enviarComandoArduino(serverSocket, receivePacket.getAddress(),
+                    comandos[random.nextInt(comandos.length)]);
+
+        }
+    }
+
+}
---
+++ sketch_esp_arduino/sketch_esp_arduino.ino
@@ -0,0 +1,156 @@
+#include "WiFiEsp.h"
+#include "WiFiEspUdp.h"
+#include "SoftwareSerial.h"
+//#include "Ultrasonic.h"
+
+//********************** Declaração de Variáveis Globais **********************
+/** ARDUINO **/
+#define CH_PD 5 //sinal de controle do CH_PD
+#define RST 6 //pino de controle do RESSET
+SoftwareSerial Serial1(3, 4); // (RX, TX) Faz a simulação do Serial1
+
+/*** ULTRASSONICO **/
+#define PIN_TRIGGER 11
+#define PIN_ECHO 10
+
+/** MOTORES **/
+#define MOTOR_1 A1
+#define MOTOR_2 A2
+#define MOTOR_3 A3
+
+/** CONTRALADOR DOS MOTORES **/
+#define ON_MOTOR_1 "ON_MOTOR_1"
+#define ON_MOTOR_2 "ON_MOTOR_2"
+#define ON_MOTOR_3 "ON_MOTOR_3"
+#define OFF_MOTOR_1 "OFF_MOTOR_1"
+//defina outras constantes de acionamento aqui
+
+/*** GLOBAIS **/
+char ssid[] = "GVT";                   // SSID da rede
+char pass[] = "17151715";             // senha do SSID
+char ipSocket[] = "192.168.25.124";  // IP utilizado no SOCKET
+unsigned int serverPort = 9876;     // porta do servidor
+unsigned int localPort  = 9875;    // porta de recebimento (client)
+char packetBuffer[255];           // Buffer para processar os pacotes recebidos
+int status = WL_IDLE_STATUS;     // Constante definida pela biblioteca, status AGUARDANDO_CONEXAO
+WiFiEspUDP Udp;                 //Instancia do tipo UDP implementado pela biblioteca
+//Ultrasonic ultrasonic(PIN_TRIGGER, PIN_ECHO); //biblioteca para utilizar o utrassonico
+
+//********************** Algoritmo **********************
+
+void setup() {
+  delay(100);
+  inicializarESP();
+  //inicializarMotores(); //Método que irá setar os pinMode dos Motores
+  Serial.begin(9600);   // Incia a comunicação serial do Arduino
+  Serial.println(F(">Inicializando...")); //println(F(STRING)) é utilizado para otimização de memória
+  conectarNoWifi();   // Inicia a conexção WI-FI
+  Udp.begin(localPort);   // Inicia a conexão com o socket via UDP
+}
+
+void loop() {
+  //Pega um pacote do udp se estiver na fila
+  int packetSize = Udp.parsePacket();
+
+  // Se existir um pacote vindo do servidor, processa
+  if (packetSize) {
+    processarPacoteUDP();
+  }
+
+  //envia as informações do ultrassonico
+  enviarDadosUltrassonico();
+
+}
+
+//********************** Métodos de Inicialização **********************
+void inicializarESP() {
+  pinMode(CH_PD, OUTPUT);
+  pinMode(RST, OUTPUT);
+  digitalWrite(CH_PD, HIGH);
+  //reseta a esp para garantir que o buffer não esteja com "lixo"
+  delay(100);
+  digitalWrite(RST, LOW);
+  delay(100);
+  digitalWrite(RST, HIGH); //RST em alto, funcionamento normal
+  Serial1.begin(9600); //Inicia a comunicação serial do ESP
+  WiFi.init(&Serial1); // Inicializa a biblioteca
+}
+
+//void inicializarMototes(){}
+
+void conectarNoWifi() {
+  // Se não conseguir a comunicação com a esp, trava a execução
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println(F("Esp não inciada corretamente, tente resetar."));
+    while (true);
+  }
+
+  // Enquanto o status for diferente de "CONECTADO"
+  while ( status != WL_CONNECTED) {
+    Serial.println(F("Tentando se conectar a rede: "));
+    Serial.println(ssid);
+    // Verifica se a conexão foi bem sucedida
+    status = WiFi.begin(ssid, pass);
+  }
+  Serial.println(F("Rede conectada."));
+}
+
+//********************** Métodos de Envio **********************
+
+//** Método principal de envio dos pacotes
+void enviarDadosViaUDP(const char *dados) {
+  Udp.beginPacket(ipSocket, serverPort); //abre o socket utilizando o ip:porta
+  Udp.write(dados); //empacota e envia os bits 
+  Udp.endPacket(); //finaliza o envio
+
+  delay(1000); // ****** Aguarda um 1 segundo antes do próximo envio, remover na versão final ******
+}
+
+void enviarDadosUltrassonico() {
+  float cmMsec;
+  //long microsec = ultrasonic.timing();
+  //cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  cmMsec = 6.550; //valor simulado de distancia em cm
+  char result[3]; // Buffer para enviar o float como um char*
+  dtostrf(cmMsec, 3, 3, result);
+  enviarDadosViaUDP(result);
+}
+
+//********************** Métodos de Recebimento **********************
+
+//Processa os dados recebidos no pacote de até 255 bytes(size do buffer) 
+void processarPacoteUDP() {
+  int len = Udp.read(packetBuffer, 255);
+  if (len > 0) {
+    packetBuffer[len] = 0;  //reseta o buffer do UDP
+  }
+  
+  String comando = (String) packetBuffer;
+
+  //recebe as solicitação de comandos do servidor
+  //e envia uma resposta a partir do arduino
+
+  if (comando.equals(ON_MOTOR_1)) {
+    //digitalWrite(MOTOR_1, HIGH);
+    enviarDadosViaUDP("MOTOR 1 LIGADO");
+  }
+
+  if (comando.equals(ON_MOTOR_2)) {
+    //digitalWrite(MOTOR_2, HIGH);
+    enviarDadosViaUDP("MOTOR 2 LIGADO");
+  }
+
+  if (comando.equals(ON_MOTOR_3)) {
+    //digitalWrite(MOTOR_3, HIGH);
+    enviarDadosViaUDP("MOTOR 3 LIGADO");
+  }
+
+  if (comando.equals(OFF_MOTOR_1)) {
+    //digitalWrite(MOTOR_1, HIGH);
+    enviarDadosViaUDP("MOTOR 1 DESLIGADO");
+  }
+
+}
+
+
+
